1031ab0a706b9e9de72f2c172eebe34f7a05595d
oonoyoshiki <23jy0208`jec.ac.jp>
2024-10-23 12:41:50 +0900

    aaaaaaa

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld{
     System.out.println("HelloWorld");
     System.out.println("HelloWorld");
     System.out.println("日本電子専門学校");
-  }
+    
+
+    aaaaaaaaaaaaaaaaaaxxxxxxxxxvvvvvvvvvvvvvvvvv
 }
